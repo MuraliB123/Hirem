081b4cc7a0161afc635c187f0ddf758ca8d18f9a
jecodeg <132217853+jecodeg@users.noreply.github.com>
2023-10-23 17:39:56 +0530

    Update employee.js

--- employee.js
+++ employee.js
@@ -2,13 +2,15 @@
 
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import EmployeeForm from './EmployeeForm';
+import ProjectAssignments from './ProjectAssignments';
 
 function Employee() {
   const [assignments, setAssignments] = useState([]);
   const [creditScore, setCreditScore] = useState(0);
 
-  // Fetch and set employee assignments and credit score
   useEffect(() => {
+    // Fetch employee assignments and credit score from the server
     async function fetchData() {
       try {
         const response = await axios.get('http://your-flask-backend-url/employee-data');
@@ -22,16 +24,23 @@ function Employee() {
     fetchData();
   }, []);
 
+  const handleTakeNewProject = async () => {
+    try {
+      // Implement functionality to allow the employee to take a new project
+      // Make an API request to the server to assign a new project to the employee
+      const response = await axios.post('http://your-flask-backend-url/take-new-project');
+      // Handle the response or update the assignments state as needed
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div>
       <h1>Employee Page</h1>
-      <h2>Assignments</h2>
-      <ul>
-        {assignments.map((assignment, index) => (
-          <li key={index}>{assignment}</li>
-        ))}
-      </ul>
+      <ProjectAssignments assignments={assignments} onTakeNewProject={handleTakeNewProject} />
       <h2>Credit Score: {creditScore}</h2>
+      <EmployeeForm />
     </div>
   );
 }
