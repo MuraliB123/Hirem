46bc949ecd25c75dcd2fb308fd5ef5956fe877c4
jecodeg <132217853+jecodeg@users.noreply.github.com>
2023-10-23 17:13:02 +0530

    Create user.js

---
+++ user.js
@@ -0,0 +1,15 @@
+// User.js
+
+import React from 'react';
+
+function User() {
+  // Retrieve and display the admin-generated reports and results here
+  return (
+    <div>
+      <h1>User Page</h1>
+      {/* Display admin-generated reports and results here */}
+    </div>
+  );
+}
+
+export default User;
